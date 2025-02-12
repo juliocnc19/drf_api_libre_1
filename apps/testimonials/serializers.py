c0a1b7aae899cca0339d9f04635c8133845e52ce
juliocnc19 <juliocnietoc19@gmail.com>
2025-02-12 11:32:02 -0400

    Cambio en respuesta para tener valor de Author

--- apps/testimonials/serializers.py
+++ apps/testimonials/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Testimonial
 
 class TestimonialSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
     class Meta:
         model = Testimonial
-        fields = '__all__'
+        fields = ['author','content','created_at','is_approved']
+        
+    def get_author(self,obj):
+        return{
+            "id":obj.author.id,
+            "username":obj.author.username,
+            "first_name":obj.author.first_name,
+            "last_name":obj.author.last_name
+        }
