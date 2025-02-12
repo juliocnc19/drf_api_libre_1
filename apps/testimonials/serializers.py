df35ea4f3fc7e3933eaa7ed85d673bb702f5e19f
juliocnc19 <juliocnietoc19@gmail.com>
2025-02-12 15:12:37 -0400

    Cambio en error al crear testimonio

--- apps/testimonials/serializers.py
+++ apps/testimonials/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
 from .models import Testimonial
+from django.contrib.auth.models import User
+
 
 class TestimonialSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),  # Permite asignar un usuario al crear
+        write_only=True  # Solo para escritura, ya que usaremos otro campo para lectura
+    )
+    author_detail = serializers.SerializerMethodField()
     class Meta:
         model = Testimonial
-        fields = ['author','content','created_at','is_approved']
+        fields = ['author','author_detail','content','created_at','is_approved']
+        extra_kwargs = {'author': {'write_only': True}}
         
-    def get_author(self,obj):
+    def get_author_detail(self,obj):
         return{
             "id":obj.author.id,
             "username":obj.author.username,
